3ef9fa2ec14ec4bf006c06f952652ea78b8fae34
Joey Chen <joeychen@yahoo-inc.com>
2015-06-12 18:43:44 +0800

    forward static function to interface

---
+++ UnitTestWithStaticFunctions/UnitTestWithStaticFunctions/prod/IProfileDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestWithStaticFunctions.prod
+{
+	public interface IProfileDao
+	{
+		string GetPassword(string account);
+
+		string GetToken(string account);
+	}
+}
--- UnitTestWithStaticFunctions/UnitTestWithStaticFunctions/prod/ProfileDao.cs
+++ UnitTestWithStaticFunctions/UnitTestWithStaticFunctions/prod/ProfileDao.cs
@@ -7,29 +7,37 @@ namespace UnitTestWithStaticFunctions.prod
 {
 	public static class ProfileDao
 	{
-		//just simulate data source
-		private static Dictionary<string, string> fakePasswordDataset = new Dictionary<string, string>
-		{
-			{"joey","1234"},
-			{"demo","!@#$"},
-		};
+		private static IProfileDao _profileDao;
+
+		////just simulate data source
+		//private static Dictionary<string, string> fakePasswordDataset = new Dictionary<string, string>
+		//{
+		//	{"joey","1234"},
+		//	{"demo","!@#$"},
+		//};
 
 		internal static string GetPassword(string account)
 		{
-			if (!fakePasswordDataset.ContainsKey(account))
-			{
-				throw new Exception("account not exist");
-			}
+			// 轉接到 interface 的方法
+			return _profileDao.GetPassword(account);
 
-			return fakePasswordDataset[account];
+			//if (!fakePasswordDataset.ContainsKey(account))
+			//{
+			//	throw new Exception("account not exist");
+			//}
+
+			//return fakePasswordDataset[account];
 		}
 
 		internal static string GetToken(string account)
 		{
-			//just for demo, it's insecure
-			var seed = new Random((account.GetHashCode() + (int)DateTime.Now.Ticks) & 0x0000FFFF);
-			var result = seed.Next(0, 999999);
-			return result.ToString("000000");
+			// 轉接到 interface 的方法
+			return _profileDao.GetToken(account);
+
+			////just for demo, it's insecure
+			//var seed = new Random((account.GetHashCode() + (int)DateTime.Now.Ticks) & 0x0000FFFF);
+			//var result = seed.Next(0, 999999);
+			//return result.ToString("000000");
 		}
 	}
 }
