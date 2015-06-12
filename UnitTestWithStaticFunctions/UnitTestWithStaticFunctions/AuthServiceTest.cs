9b501f9950a00821120161fb2ef946edcb22f052
Joey Chen <joeychen@yahoo-inc.com>
2015-06-12 18:06:15 +0800

    initialized version

---
+++ UnitTestWithStaticFunctions/UnitTestWithStaticFunctions/AuthServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestWithStaticFunctions.prod;
+using Shouldly;
+
+namespace UnitTestWithStaticFunctions
+{
+	[TestClass]
+	public class AuthServiceTest
+	{
+		[TestMethod]
+		public void Test_IsValid_joey_1234666666_Should_Return_True()
+		{
+			var target = new AuthService();
+			var account = "joey";
+			var password = "1234666666";
+			
+			var actual = target.IsValid(account, password);
+
+			var expected = true;
+
+			actual.ShouldBe(expected);
+		}
+	}
+}
---
+++ UnitTestWithStaticFunctions/UnitTestWithStaticFunctions/prod/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestWithStaticFunctions.prod
+{
+	public class AuthService
+	{
+		public bool IsValid(string account, string password)
+		{
+			string passwordFromDao = ProfileDao.GetPassword(account);
+			string token = ProfileDao.GetToken(account);
+
+			var validPassword = passwordFromDao + token;
+
+			var isValid = validPassword == password;
+			return isValid;
+		}
+	}
+}
